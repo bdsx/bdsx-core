287a621e44a0a01c35dee9bc9f6c58faa244ac85
karikera <karikera3@hotmail.com>
2021-09-14 13:31:12 +0900

    node-chakracore-build - bypass the msbuild bug .gitmodules - define branch bdsx-core.sln - remove Win32 build of node-chakracore node-chakracore - exclude node.exe

--- node-chakracore-build/build.bat
+++ node-chakracore-build/build.bat
@@ -3,37 +3,39 @@ set SolutionDir=%~1
 set Config=%~2
 set OutDir=%~3
 
-set node=%SolutionDir%node-chakracore\%Config%\node.lib
+set targetdir=%SolutionDir%node-chakracore\%Config%
+set node=%targetdir%\node.lib
 
 call :check_output
 if %found%==1 (
 	echo node-chakracore passed.
-	type NUL > "%OutDir%node-chakracore-done"
-	goto :eof
+	type NUL > "%targetdir%\node-chakracore-done"
+	exit /b
 )
 
-pushd "%SolutionDir%node-chakracore"
-call vcbuild.bat %Config% vs2019
+pushd "%SolutionDir%\node-chakracore"
+call vcbuild.bat %Config% vs2019 cctest
 popd
 call :check_output
 if %found%==1 (
-	type NUL > "%OutDir%node-chakracore-done"
+	type NUL > "%targetdir%\node-chakracore-done"
 )
+exit /b
 
 :check_output
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\cares.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\chakrashim.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\http_parser.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\icudata.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\icui18n.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\icustubdata.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\icutools.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\icuucx.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\libuv.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\nghttp2.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\node.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\openssl.lib" goto :notfound
-if not exist "%SolutionDir%node-chakracore\%Config%\lib\zlib.lib" goto :notfound
+if not exist "%targetdir%\lib\cares.lib" goto :notfound
+if not exist "%targetdir%\lib\chakrashim.lib" goto :notfound
+if not exist "%targetdir%\lib\http_parser.lib" goto :notfound
+if not exist "%targetdir%\lib\icudata.lib" goto :notfound
+if not exist "%targetdir%\lib\icui18n.lib" goto :notfound
+if not exist "%targetdir%\lib\icustubdata.lib" goto :notfound
+if not exist "%targetdir%\lib\icutools.lib" goto :notfound
+if not exist "%targetdir%\lib\icuucx.lib" goto :notfound
+if not exist "%targetdir%\lib\libuv.lib" goto :notfound
+if not exist "%targetdir%\lib\nghttp2.lib" goto :notfound
+if not exist "%targetdir%\lib\node.lib" goto :notfound
+if not exist "%targetdir%\lib\openssl.lib" goto :notfound
+if not exist "%targetdir%\lib\zlib.lib" goto :notfound
 set found=1
 exit /b
 :notfound
